9b070bb080795f93f4021b83bd8b2747cdda7b78
SGRLAB_INFRARED2 <sgrlabinfrared2@gmail.com>
2020-09-23 15:35:57 -0400

    Added 64 bit support for IO interface

---
+++ spectrometer/IO_Interface_64.m
@@ -0,0 +1,52 @@
+classdef IO_Interface_64 < handle
+    
+    properties (SetAccess = private)
+        dio;
+        active;
+    end
+    
+    methods
+        
+        function obj = IO_Interface_64
+            obj.active = 0;
+            try 
+%                 obj.dio = digitalio('nidaq', 'Dev2');
+                obj.dio = daq.createSession('ni');
+                obj.active = 1;
+            catch
+                warning('Spectrometer:DIO', 'Digital I/O module not found.  Entering simulation mode');
+            end
+            if obj.active
+                warning('off', 'daq:Session:onDemandOnlyChannelsAdded')
+                addDigitalChannel(obj.dio,'Dev3','Port1/Line0','OutputOnly');
+%                 addline(obj.dio, 7, 1, 'out');      % Port 1 bit 7
+            end
+        end
+        
+        function delete(obj)
+            CloseClockGate(obj);
+            if obj.active
+                delete(obj.dio);
+            end
+        end
+
+        function OpenClockGate(obj)
+            if obj.active
+                outputSingleScan(obj.dio, 1)
+%                 putvalue(obj.dio.Line(1), 1);
+            end
+        end
+        
+        function CloseClockGate(obj)
+            if obj.active
+                outputSingleScan(obj.dio, 0)
+%                 putvalue(obj.dio.Line(1), 0);
+            end
+        end
+        
+    end
+    
+end
+            
+            
+            
